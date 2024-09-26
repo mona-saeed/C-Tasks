38cdcf0926bf93b203fd3b6ff71e561ed48cb5d1
mona saeed <mona.sud86>
2024-09-27 02:27:01 +0300

    task1 C#

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(number % 2 == 0 ? "Even" : "Odd");  
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //bool h = (bool)'o';
+        //Console.WriteLine(h);
+        //bool.TryParse("k", out bool x);
+        //Console.WriteLine(x);
+        //Console.WriteLine(GetExperienceLevel(2));
+
+        Console.Write("Enter score: ");
+        int.TryParse(Console.ReadLine(), out int score);
+        while(score < 0 || score > 100)
+        {
+            Console.Write("Enter valid score: ");
+            int.TryParse(Console.ReadLine(), out score);
+        }
+        string grade = score switch
+        {
+            >= 90 and <= 100 => "A",
+            >= 80 and < 90 => "B",
+            >= 70 and < 80 => "C",
+            >= 60 and < 70 => "D",
+            >= 0  and < 60 => "F",
+            _ => "Invalid score"
+        };
+            Console.WriteLine($"The grade is {grade}");
+    }
+}
